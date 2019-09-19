3c3b9696bb421758edadb1bb8e0b272790013a55
Anleo04 <55521243+Anleo04@users.noreply.github.com>
2019-09-18 22:46:04 -0500

    Update en.php

--- language/en.php
+++ language/en.php
@@ -13,3 +13,44 @@ $tema1desc="The theory and relevancy of Pascal's law in daily life";
 $publicado="Posted by";
 $agradecimientos="Special thanks to <a href='https://startbootstrap.com/themes/'> StartBootstrap</a> for the template.";
 $tomado="Taken from";
+$p1Pascal="PASCAL´S PRINCIPLE
+The pascal principle is a law or a statement that was elaborated by the French physicist and mathematician Blas Pascal and that tells us that the increased pressure that is applied to a surface that has an incompressible fluid that is contained in an indeformed vessel e, it is transmitted with equal value to each of the parts that the vessel has.
+";
+$p2Pascal="WHAT IS THE PASCAL´S PRINCIPLE?
+This principle or law of the branch of physics tells us that when a certain pressure is applied to a fluid that is in a container, the same value will be distributed to each of the parties that conform it.
+";
+$p3Pascal="Formula
+The Pascal´s principle can be used in jobs that need a great deal of effort but at the same time must be done by applying a small force, for example:
+-Hydraulic brakes
+-Tyres that cars have
+-Hydraulic lifts
+-Hydraulic presses
+";
+$p4Pascal="Importance
+The importance of this principle lies in the relationship between the pressure and depth of a fluid, or in the height of the fluid column, from a reference value. It is important for its validity in the case of compressible fluids that vary with pressure.
+";
+$p5Pascal="Experiment
+The following is an example that can be applied to explain the Pascal´s principle.
+Materials
+-A transparent container
+-1 large balloon
+-1 scissors blunt tip
+-1 glass dropper
+-Water
+Instructions
+First we'll take the scissors and cut the balloon nozzle. We fill it with water to the container, leaving it almost full. Place the empty dropper in the container and close the container with the balloon so that the surface of the balloon is tight. Then we push the surface of the balloon down and notice that the dropper fills with water and sinks.
+";
+$p6Pascal="EXAMPLES OF PASCAL´S PRINCIPLE
+An example of Pascal's principle is as follows:
+Calculate the force and pressure exerted on a plunger, if we know that the resulting force is 42N, the larger plunger has a radius of 55 centimeters and the minor plunger has a radius of 22 centimeters.
+-Larger embolo: (3.14) (0.552) ? (3.14)(0.3025) s 0.950 m2
+-Smaller embolo: (3.14) (0.222) ? (3.14)(0.0484) ? 0.152 m2
+We calculate the pressure:
+-F2-p2S2
+So:
+-p2-F2/S2
+-p2-42/0.95 -44.21 Pa
+Calculate the applied force:
+-F1-p1S1
+-F1o (44.21) (0.152) - 72 N
+";
