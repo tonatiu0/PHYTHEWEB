e3ecd04fa9a06e24aad424c76a268d3079af4d35
ErnestoRB <55329286+ErnestoRB@users.noreply.github.com>
2019-09-19 18:02:23 -0500

    Update es.php

--- language/es.php
+++ language/es.php
@@ -13,32 +13,38 @@ $tema1desc="La teoría e importancia del principio de Pascal en la vida cotidian
 $publicado="Publicado por";
 $agradecimientos="Gracias a <a href='https://startbootstrap.com/themes/'> StartBootstrap</a> por la plantilla.";
 $tomado="Tomado de";
-$p1Pascal="PRINCIPIO DE PASCAL
+$p1Pascal="<b>PRINCIPIO DE PASCAL</b><br>
 El principio de Pascal es una ley o un enunciado que fue elaborado por el físico y matemático de origen francés Blas Pascal y que nos dice que el aumento de la presión que se aplica a una superficie que tiene un fluido incompresible que está contenido en un recipiente indeformable, se transmite con igual valor a cada una de las partes que tenga el recipiente.
 ";
-$p2Pascal="¿Qué ES EL PRINCIPIO DE PASCAL?
+$p2Pascal="<b>¿Qué ES EL PRINCIPIO DE PASCAL?</b><br>
 Este principio o ley de la rama de la física nos dice que cuando se aplica una determinada presión a un fluido que está en un recipiente, el mismo valor será repartido a cada una de las partes que lo conformen.
 ";
-$p3Pascal="FÓRMULA
+$p3Pascal="<b>FÓRMULA</b><br>
 El principio de Pascal se puede utilizar en trabajos que necesitan de un gran esfuerzo pero que al mismo tiempo deben ser realizados aplicando una fuerza pequeña, por ejemplo:
--Frenos hidráulicos
--Neumáticos que tienen los automóviles
--Elevadores hidráulicos
+<br>-Frenos hidráulicos<br>
+-Neumáticos que tienen los automóviles<br>
+-Elevadores hidráulicos<br>
 -Prensas hidráulicas
+    <div class="col align-self-center">
+      <img src="img/formulaPascal.png"/>
+    </div>
 ";
-$p4Pascal="IMPORTANCIA
+$p4Pascal="<b>IMPORTANCIA</b><br>
 La importancia de este principio radica en la relación que hay entre la presión y la profundidad de un fluido, o en la altura de columna de fluido, a partir de un valor de referencia. Es importante por su validez en el caso de fluidos compresibles que varían con la presión.
 ";
-$p5Pascal="EXPERIMENTO
-El siguiente es un ejemplo que puede ser aplicado para explicar el principio de Pascal.
-Materiales
--Un recipiente transparente
--1 globo grande
--1 tijera punta roma
--1 gotero de cristal
--Agua
-Instrucciones
-Primero tomaremos la tijera y cortamos la boquilla del globo. Llenamos con agua al recipiente, dejándolo casi lleno. Introducimos el gotero vacío en el recipiente y cerramos el recipiente con el globo de manera que la superficie del globo quede tirante. Luego empujamos la superficie del globo hacia abajo y observamos que el gotero se llena de agua y se hunde.
+$p5Pascal="<b>EXPERIMENTO</b><br>
+El siguiente es un ejemplo que puede ser aplicado para explicar el principio de Pascal.<br>
+Materiales<br>
+-Un recipiente transparente<br>
+-1 globo grande<br>
+-1 tijera punta roma<br>
+-1 gotero de cristal<br>
+-Agua<br><br>
+Instrucciones:<br>
+Primero tomaremos la tijera y cortamos la boquilla del globo. <br>
+Llenamos con agua al recipiente, dejándolo casi lleno. <br>
+Introducimos el gotero vacío en el recipiente y cerramos el recipiente con el globo de manera que la superficie del globo quede tirante. <br>
+Luego empujamos la superficie del globo hacia abajo y observamos que el gotero se llena de agua y se hunde.<br>
 ";
 $p6Pascal="EJEMPLOS DEL PRINCIPIO DE PASCAL
 Un ejemplo del principio de Pascal es el siguiente:
