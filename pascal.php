d863a9daa11d5efa6c81f7cbd1e580281247ae41
ErnestoRB <55329286+ErnestoRB@users.noreply.github.com>
2019-09-20 14:33:16 -0500

    Añadir secciones, iconos, y multimedia

--- pascal.php
+++ pascal.php
@@ -11,8 +11,7 @@ require 'requirelanguage.php';
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
-  <title>Fisica II</title>
+  <title><?php echo $fisica; ?> II</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -32,7 +31,7 @@ require 'requirelanguage.php';
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="index.php"><?php echo $fisica; ?></a>
+      <a class="navbar-brand" href="index.php"><?php echo $fisica; ?>  <i class="fas fa-bolt"></i></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -44,7 +43,7 @@ require 'requirelanguage.php';
           </li>
           <li class="nav-item">
             <div class="dropdown closed">
-              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-globe"></i>
               <?php echo $cambiarIdioma; ?>
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
@@ -72,7 +71,7 @@ require 'requirelanguage.php';
             <h1><?php echo $tema1 ?> <span class="badge badge-danger"><?php echo $new ?></span></h1>
          <!--   <h2 class="subheading">Problems look mighty small from 150 miles up</h2> -->
             <span class="meta"><?php echo $publicado ?>
-              <a href="#">Admin</a> |
+              <a href="https://github.com/ErnestoRB/PHYTHEWEB">Admin</a> |
               7 Septiembre 2019</span>
           </div>
         </div>
@@ -85,23 +84,68 @@ require 'requirelanguage.php';
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-
-        <!--  <h2 class="section-heading">The Final Frontier</h2>
-
-          <blockquote class="blockquote">The dreams of yesterday are the hopes of today and the reality of tomorrow. Science has not yet mastered prophecy. We predict too much for the next year and yet far too little for the next ten.</blockquote> -->
-
+          <h2 class='display-4 text-center'><b><?php echo $tema1 ?></b></h2>
           <p><?php echo $p1Pascal ?></p>
+          <hr class="w-75 bg-dark">
           <p><?php echo $p2Pascal ?></p>
+          <hr class="w-75 bg-dark">
           <p><?php echo $p3Pascal ?></p>
-          <div class="col align-self-center">
-          <img src="img/formulaPascal.png"/>
+          <div class="alert alert-light shadow-lg w-50" role="alert" style="margin: auto;">
+            <img class="img-fluid" src="img/formulaPascal.png"/>
           </div>
+          <hr class="w-75 bg-dark">
           <p><?php echo $p4Pascal ?></p>
+          <hr class="w-75 bg-dark">
           <p><?php echo $p5Pascal ?></p>
+          <hr class="w-75 bg-dark">
           <p><?php echo $p6Pascal ?></p>
-
-          <iframe src="https://phet.colorado.edu/sims/html/under-pressure/latest/under-pressure_es.html" width="800" height="600" scrolling="no" allowfullscreen></iframe>  
+          <hr class="w-75 bg-dark">
+          <h3 class="text-center"><?php echo $appletP ?></h3><br>
+          <iframe class="container-fluid" src="https://phet.colorado.edu/sims/html/under-pressure/latest/under-pressure_es.html" width="800" height="600" scrolling="no" allowfullscreen></iframe>  
           <span class="caption text-muted">Applet <?php echo $tomado ?> <a href='https://phet.colorado.edu/es/simulation/under-pressure'>PHET</a></span>
+          <hr class="w-75 bg-dark">
+          <h3 class="text-center">Multimedia</h3><br>
+          <!-- Carousel -->
+          <div class="bd-example">
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="img/pascal1.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo $imgP1 ?></h5>
+                    <p><?php echo $imgP1D ?></p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="img/pascal2.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo $imgP2 ?></h5>
+                    <p><?php echo $imgP2D ?></p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="img/pascal3.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo $imgP3 ?></h5>
+                    <p><?php echo $imgP3D ?></p>
+                  </div>
+                </div>
+              <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+
           <!--
           <a href="#">
             <img class="img-fluid" src="img/post-sample-image.jpg" alt="">
@@ -123,7 +167,7 @@ require 'requirelanguage.php';
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
 
-          <p class="copyright text-muted">Copyright &copy; Your Website 2019</p>
+          <p class="copyright text-muted">Copyright &copy; El combo 2019</p>
           <p class="copyright text-muted"> <?php echo $agradecimientos ?> </p>
 
         </div>
