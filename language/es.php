3f44e7cf0c702cb36f014e3f304cb9d56a47795c
ErnestoRB <55329286+ErnestoRB@users.noreply.github.com>
2019-09-20 14:31:27 -0500

    Actualización de Español
    Casi final

--- language/es.php
+++ language/es.php
@@ -14,48 +14,61 @@ $tema1desc="La teoría e importancia del principio de Pascal en la vida cotidian
 $publicado="Publicado por";
 $agradecimientos="Gracias a <a href='https://startbootstrap.com/themes/'> StartBootstrap</a> por la plantilla.";
 $tomado="Tomado de";
-$p1Pascal="<b>PRINCIPIO DE PASCAL</b><br>
-El principio de Pascal es una ley o un enunciado que fue elaborado por el físico y matemático de origen francés Blas Pascal y que nos dice que el aumento de la presión que se aplica a una superficie que tiene un fluido incompresible que está contenido en un recipiente indeformable, se transmite con igual valor a cada una de las partes que tenga el recipiente.
+$p1Pascal="
+El principio de Pascal es una ley o un enunciado que fue elaborado por el físico y matemático de origen francés <mark class='bg-light shadow'>Blas Pascal</mark> y que nos dice que el aumento de la presión que se aplica a una superficie que tiene un fluido incompresible que está contenido en un recipiente indeformable, se transmite con igual valor a cada una de las partes que tenga el recipiente.
 ";
-$p2Pascal="<b>¿Qué ES EL PRINCIPIO DE PASCAL?</b><br>
+$p2Pascal="<h4>¿Qué ES EL PRINCIPIO DE PASCAL?</h4><br>
 Este principio o ley de la rama de la física nos dice que cuando se aplica una determinada presión a un fluido que está en un recipiente, el mismo valor será repartido a cada una de las partes que lo conformen.
 ";
-$p3Pascal="<b>FÓRMULA</b><br>
-El principio de Pascal se puede utilizar en trabajos que necesitan de un gran esfuerzo pero que al mismo tiempo deben ser realizados aplicando una fuerza pequeña, por ejemplo:
-<br>-Frenos hidráulicos<br>
--Neumáticos que tienen los automóviles<br>
--Elevadores hidráulicos<br>
--Prensas hidráulicas
-
+$p3Pascal="<h4>FÓRMULA <i class='fas fa-square-root-alt'></i></h4><br>
+El principio de Pascal se puede utilizar en trabajos que necesitan de un gran esfuerzo pero que al mismo tiempo deben ser realizados aplicando una fuerza pequeña, por ejemplo:<br>
+<ul>
+<li>Frenos hidráulicos</li>
+<li>Neumáticos que tienen los automóviles</li>
+<li>Elevadores hidráulicos</li>
+<li>Prensas hidráulicas</li>
+</ul>
 ";
-$p4Pascal="<b>IMPORTANCIA</b><br>
+$p4Pascal="<h4>IMPORTANCIA <i class='fas fa-exclamation'></i></h4><br>
 La importancia de este principio radica en la relación que hay entre la presión y la profundidad de un fluido, o en la altura de columna de fluido, a partir de un valor de referencia. Es importante por su validez en el caso de fluidos compresibles que varían con la presión.
 ";
-$p5Pascal="<b>EXPERIMENTO</b><br>
-El siguiente es un ejemplo que puede ser aplicado para explicar el principio de Pascal.<br>
-Materiales<br>
--Un recipiente transparente<br>
--1 globo grande<br>
--1 tijera punta roma<br>
--1 gotero de cristal<br>
--Agua<br><br>
-Instrucciones:<br>
-Primero tomaremos la tijera y cortamos la boquilla del globo. <br>
-Llenamos con agua al recipiente, dejándolo casi lleno. <br>
-Introducimos el gotero vacío en el recipiente y cerramos el recipiente con el globo de manera que la superficie del globo quede tirante. <br>
-Luego empujamos la superficie del globo hacia abajo y observamos que el gotero se llena de agua y se hunde.<br>
+$p5Pascal="<h4>EXPERIMENTO <i class='fas fa-paper-plane'></h4></i><br>
+El siguiente es un ejemplo que puede ser aplicado para explicar el principio de Pascal.<br><br>
+<em>Materiales
+<ul>
+<li>Un recipiente transparente</li>
+<li>1 globo grande</li>
+<li>1 tijera punta roma</li>
+<li>1 gotero de cristal</li>
+<li>Agua</li>
+</ul><br>
+
+Instrucciones:<ol>
+<li>Primero tomaremos la tijera y cortamos la boquilla del globo. </li>
+<li>Llenamos con agua al recipiente, dejándolo casi lleno. </li>
+<li>Introducimos el gotero vacío en el recipiente y cerramos el recipiente con el globo de manera que la superficie del globo quede tirante.</li>
+<li>Luego empujamos la superficie del globo hacia abajo y observamos que el gotero se llena de agua y se hunde.</li></ol></em>
 ";
-$p6Pascal="EJEMPLOS DEL PRINCIPIO DE PASCAL
-Un ejemplo del principio de Pascal es el siguiente:
-Calcular la fuerza y la presión ejercida en un émbolo, si sabemos que la fuerza resultante es de 42N, el émbolo mayor tiene un radio de 55 centímetros y el émbolo menor tiene un radio de 22 centímetros.
--Embolo mayor: (3.14) (0.55²) = (3.14)(0.3025) = 0.950 m²
--Embolo menor: (3.14) (0.22²) = (3.14)(0.0484) = 0.152 m²
-Calculamos la presión:
--F2=p2S2
-Por lo que:
--p2= F2/S2
--p2= 42/0.95 = 44.21 Pa
-Calculamos la fuerza aplicada:
--F1= p1S1
--F1= (44.21) (0.152) = 72 N
+$p6Pascal="<h4>EJEMPLOS DEL PRINCIPIO DE PASCAL</h4><br>
+Un ejemplo del principio de Pascal es el siguiente:<br>
+<em>
+<b>1.</b> Calcular la fuerza y la presión ejercida en un émbolo, si sabemos que la fuerza resultante es de 42N, el émbolo mayor tiene un radio de 55 centímetros y el émbolo menor tiene un radio de 22 centímetros.</em><br>
+<b>Datos:</b><br>
+-Embolo mayor: (3.14) (0.55²) = (3.14)(0.3025) = 0.950 m²<br>
+-Embolo menor: (3.14) (0.22²) = (3.14)(0.0484) = 0.152 m²<br>
+Calculamos la presión:<br>
+<var>F<sub>2</sub>=p<sub>2</sub>S<sub>2</sub></var><br>
+Por lo que:<br>
+<var>p<sub>2</sub>= F<sub>2</sub>/S<sub>2</sub></var><br>
+<var>p<sub>2</sub>= 42/0.95 = <b>44.21 Pa</b></var><br>
+Calculamos la fuerza aplicada:<br>
+<var>F<sub>1</sub>= p<sub>1</sub>S<sub>1</sub></var><br>
+<var>F<sub>1</sub>= (44.21) (0.152) = <b>72 N</b></var><br>
 ";
+$appletP="Applet para entender el tema";
+$imgP1="Retroexcavadora";
+$imgP1D="Una retroexcavadora hace uso del principio de Pascal para levantar grandes pesos.";
+$imgP2="Gato hidráulico";
+$imgP2D="Se usa para poder levantar el automóvil sin tanta fuerza.";
+$imgP3="Elevadora";
+$imgP3D="Se usa el principio para poder subir la plataforma.";
